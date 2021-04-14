11646ff25cdf53f7ba38bd0a7e2f2b8ac8d62122
supran2811 <supransengupta@gmail.com>
2021-04-14 07:11:41 +0530

    update github workflow

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -1,26 +1,23 @@
 const { merge } = require('webpack-merge');
-
 const commonConfig = require('./webpack.common');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const packageJson = require('../package.json');
 
-const devConfig = {
-  mode: 'development',
-  devServer: {
-    port: 8080,
-    historyApiFallback: {
-      'index': 'index.html'
-    }
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: "[name].[contenthash].js"
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'container',
-      remotes: {
-        'marketing': 'marketing@http://localhost:8081/remoteEntry.js'
+      name: 'marketing',
+      filename:'remoteEntry.js',
+      exposes: {
+        './MarketingApp': "./src/bootstrap"
       },
       shared: packageJson.dependencies
     })
   ]
 }
 
-module.exports = merge(commonConfig,devConfig);
+module.exports = merge(commonConfig,prodConfig);
