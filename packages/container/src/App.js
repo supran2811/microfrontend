9114510e576f6316a4ce1077952e32289732ea9e
supran2811 <supransengupta@gmail.com>
2021-04-15 07:14:26 +0530

    updating container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Marketing from './components/Marketing';
 const App = () => {
-  return (<div><h1>Hi There!!!</h1>
+  return (<div><h1>Hi There!</h1>
   <hr />
   <Marketing />
   </div>)
